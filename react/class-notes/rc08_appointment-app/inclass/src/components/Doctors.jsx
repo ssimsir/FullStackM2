319466685028403028f3bbdff8edc0bf0c7d4dce
ssimsir <s_simsir@yahoo.com>
2024-04-01 22:40:15 +0300

    img click open modal

--- react/class-notes/rc08_appointment-app/inclass/src/components/Doctors.jsx
+++ react/class-notes/rc08_appointment-app/inclass/src/components/Doctors.jsx
@@ -1,13 +1,39 @@
-import Container from "react-bootstrap/Container"
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+import { doctorData } from "../helpers/data";
+import AddModal from "./AddModal";
+import { useState } from "react";
 
 const Doctors = () => {
+  // console.log(doctorData)
+  const [showModal, setShow] = useState(false);
   return (
     <Container>
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Our Doctors
       </h3>
+      <Row>
+        {/* {doctorData.map((doctor) => {
+          const { id, img, name, dep } = doctor;
+          return (
+            <Col key={id}>
+              <Image src={img} alt={name} />
+            </Col>
+          );
+        })} */}
+        {doctorData.map(({ id, img, name, dep }) => (
+          <Col xs={6} sm={4} lg={3} key={id}>
+            <Image className="img-thumbnail doctor-img w-100" src={img} alt={name} onClick={() => setShow(true)}/>
+            <h5>{name}</h5>
+            <p>{dep}</p>
+          </Col>
+        ))}
+      </Row>
+      <AddModal showModal={showModal} handleClose={()=>setShow(false)}/>
     </Container>
-  )
-}
+  );
+};
 
-export default Doctors
+export default Doctors;
