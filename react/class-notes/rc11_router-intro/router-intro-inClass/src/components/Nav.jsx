165ac122fec37a8aa4154f8659d9005367e07dd1
ssimsir <s_simsir@yahoo.com>
2024-04-17 11:45:15 +0300

    route finisihed

--- react/class-notes/rc11_router-intro/router-intro-inClass/src/components/Nav.jsx
+++ react/class-notes/rc11_router-intro/router-intro-inClass/src/components/Nav.jsx
@@ -1,14 +1,16 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Nav = () => {
   return (
     <ul>
       <li>
-        <a href="/">Home</a>
+        <Link to="/">Home</Link>
       </li>
       <li>
-        <a href="/instructors">Instructors</a>
+        <Link to="/instructors">Instructors</Link>
       </li>
       <li>
-        <a href="/contact">Contact</a>
+        <NavLink to="/contact">Contact</NavLink>
       </li>
     </ul>
   );
