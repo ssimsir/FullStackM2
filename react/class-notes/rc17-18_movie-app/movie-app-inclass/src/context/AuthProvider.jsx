8f80bba7a551b1c5823a9ed3a428f1f4d06e5807
ssimsir <s_simsir@yahoo.com>
2024-04-28 23:53:29 +0300

    login done

--- react/class-notes/rc17-18_movie-app/movie-app-inclass/src/context/AuthProvider.jsx
+++ react/class-notes/rc17-18_movie-app/movie-app-inclass/src/context/AuthProvider.jsx
@@ -1,33 +1,48 @@
 import React, { createContext, useContext, useState } from "react";
 import { auth } from "../auth/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 export const useAuthContext = () => {
-   return useContext(AuthContext)
-}
+	return useContext(AuthContext);
+};
 
 const AuthProvider = ({ children }) => {
+	const [currentUser, setCurrentUser] = useState(false);
 
-   const [currentUser, setCurrentUser] = useState(false)
-   
-   const navigate = useNavigate()
+	const navigate = useNavigate();
 
-   const createUser = async(email, password) => {
-      try {
-         const userCredential = await createUserWithEmailAndPassword(auth, email, password)
-         navigate("/login")
-         console.log(userCredential)
-      } catch (error) {
-         console.log(error)
-      }
+	const createUser = async (email, password) => {
+		try {
+			const userCredential = await createUserWithEmailAndPassword(
+				auth,
+				email,
+				password
+			);
+			navigate("/login");
+			console.log(userCredential);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-      
-   }
+   const signIn = async (email, password) => {
+		try {
+			const userCredential = await signInWithEmailAndPassword(
+				auth,
+				email,
+				password
+			);
+			navigate("/");
+			console.log(userCredential);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-	const values = {currentUser, createUser};
+	const values = { currentUser, createUser, signIn};
 	return (
 		<AuthContext.Provider value={values}>{children}</AuthContext.Provider>
 	);
